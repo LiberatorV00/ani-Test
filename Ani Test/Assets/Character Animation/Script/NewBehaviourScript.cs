090297819a2eefddf904da564ec40d51e380fda1
LiberatorV00 <muhd01marzuqi@Gmail.com>
2020-11-11 17:01:37 +0800

    Ani

---
+++ Ani Test/Assets/Character Animation/Script/NewBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Animator playerAnim;
+    float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            playerAnim.SetBool("iStrafe", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnim.SetBool("iStrafe", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnim.SetTrigger("iAtk");
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("CubeBreak"))
+        {
+            playerAnim.SetTrigger("Death");
+        }
+    }
+}
